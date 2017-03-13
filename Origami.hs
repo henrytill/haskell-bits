dfdffea4474b49bc0816807324967621b4c36faa
Henry Till <henrytill@gmail.com>
2017-03-13 01:51:32 -0400

    update Origami.hs

--- Origami.hs
+++ Origami.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 -- |
 -- Module      : Origami
@@ -9,6 +10,8 @@
 -- <https://www.cs.ox.ac.uk/jeremy.gibbons/publications/origami.pdf>
 --
 module Origami where
+
+import Data.Maybe (isNothing)
 
 -- * Origami with lists: sorting
 
@@ -174,4 +177,125 @@ unfoldL
   -> (b -> b)    -- ^ When @p == False@, `g` gives the seed from which to unfold the tail
   -> b           -- ^ The thing to unfold
   -> List a      -- ^ The unfolded value
-unfoldL p f g b = if p b then Nil else Cons (f b) (unfoldL p f g (g b))
+unfoldL p f g b = if p b
+                    then Nil
+                    else Cons (f b) (unfoldL p f g (g b))
+
+-- *** Exercise 3.6
+-- $ Express `unfoldL` in terms of `unfoldL'`, and vice versa
+
+unfoldL1
+  :: forall a b
+   . (b -> Bool)
+  -> (b -> a)
+  -> (b -> b)
+  -> b
+  -> List a
+unfoldL1 p f g = unfoldL' translator
+  where
+    translator :: b -> Maybe (a, b)
+    translator x | p x       = Nothing
+                 | otherwise = Just (f x, g x)
+
+unfoldL2 :: forall a b. (b -> Maybe (a, b)) -> b -> List a
+unfoldL2 h = unfoldL p f g
+  where
+    p :: b -> Bool
+    p = isNothing . h
+
+    f :: b -> a
+    f x = case h x of
+            Just (a, _) -> a
+            Nothing     -> error "something is wrong"
+
+    g :: b -> b
+    g x = case h x of
+            Just (_, b) -> b
+            Nothing     -> error "something is wrong"
+
+-- $
+-- =Note:
+-- Conversely, one could define a function `foldL'` taking a single argument of
+-- type @Maybe (a, b) -> b@ in place of `foldL`'s two argument:
+
+foldL' :: (Maybe (a, b) -> b) -> List a -> b
+foldL' f Nil         = f Nothing
+foldL' f (Cons x xs) = f (Just (x, foldL' f xs))
+
+-- *** Exercise 3.8
+-- $ Define `foldL'` in terms of `foldL`, and vice versa.
+
+-- foldL :: (a -> b -> b) -> b -> List a -> b
+
+foldL1 :: forall a b. (Maybe (a, b) -> b) -> List a -> b
+foldL1 f = foldL translator zero
+  where
+    translator :: a -> b -> b
+    translator a b = f (Just (a, b))
+    zero           = f Nothing
+
+foldL2 :: forall a b. (a -> b -> b) -> b -> List a -> b
+foldL2 f zero = foldL' translator
+  where
+    translator :: Maybe (a, b) -> b
+    translator (Just (a, b)) = f a b
+    translator Nothing       = zero
+
+-- *** Exercise 3.9
+-- $ The adaptation of the single-argument fold and unfold to the multi-argument
+-- interface is simplified by functions of the following types:
+
+foldLargs :: forall a b. (a -> b -> b) -> b -> (Maybe (a, b) -> b)
+foldLargs f zero = translator
+  where
+    translator :: Maybe (a, b) -> b
+    translator (Just (a, b)) = f a b
+    translator Nothing       = zero
+
+unfoldLargs
+  :: forall a b
+   . (b -> Bool)
+  -> (b -> a)
+  -> (b -> b)
+  -> (b -> Maybe (a, b))
+unfoldLargs p f g = translator
+  where
+    translator :: b -> Maybe (a, b)
+    translator x | p x       = Just (f x, g x)
+                 | otherwise = Nothing
+
+-- $
+-- One sorting algorithm expressible as a list unfold is /selection sort/, which
+-- operates by at each step removing the minimum element of the list to be
+-- sorted, but leaving the other elements in the same order.  We first define
+-- the function /delmin/  to do this removal:
+
+delmin :: Ord a => List a -> Maybe (a, List a)
+delmin Nil = Nothing
+delmin xs  = Just (y, deleteL y xs)
+  where
+    y = minimumL xs
+
+-- $
+-- `minimumL` and `deleteL` are `List` equivalents of the standard library
+-- functions `minimum` and `delete`:
+
+minimumL :: Ord a => List a -> a
+minimumL Nil         = error "minimumL Nil"
+minimumL (Cons x xs) = foldL min x xs
+
+deleteL :: Eq a => a -> List a -> List a
+deleteL _ Nil         = Nil
+deleteL y (Cons x xs)
+  | y == x            = xs
+  | otherwise         = Cons x (deleteL y xs)
+
+-- $
+-- Then selection sort is straightforward to define:
+
+-- |
+-- >>> ssort (Cons 6 (Cons 1 (Cons 5 (Cons 2 (Cons 4 (Cons 3 Nil))))))
+-- Cons 1 (Cons 2 (Cons 3 (Cons 4 (Cons 5 (Cons 6 Nil)))))
+--
+ssort :: Ord a => List a -> List a
+ssort = unfoldL' delmin
