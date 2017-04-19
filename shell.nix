c733b59a55901961ed7e882ca7b3f0a80de42a59
Henry Till <henrytill@gmail.com>
2017-04-19 01:09:19 -0400

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,15 @@
 { nixpkgs ? import <nixpkgs> {} }:
 let
   inherit (nixpkgs) pkgs;
-  ghc = pkgs.haskellPackages.ghcWithHoogle (ps: with ps; [
-          doctest
-          mtl
-          random
-        ]);
+  ghcEnv = pkgs.haskellPackages.ghcWithHoogle (ps: with ps; [
+             doctest
+             lens
+             mtl
+             random
+           ]);
 in
 pkgs.stdenv.mkDerivation {
   name = "playground";
-  buildInputs = [ ghc ];
-  shellHook = "eval $(egrep ^export ${ghc}/bin/ghc)";
+  buildInputs = [ ghcEnv ];
+  shellHook = "eval $(egrep ^export ${ghcEnv}/bin/ghc)";
 }
