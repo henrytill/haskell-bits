63cc3a8b357530048c0eb64b288b5e5f28622737
Henry Till <henrytill@gmail.com>
2017-08-28 23:48:21 -0400

    add MicroParsec.hs

---
+++ MicroParsec.hs
@@ -0,0 +1,171 @@
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
+
+-- |
+-- <http://dev.stephendiehl.com/fun/002_parsers.html>
+--
+module MicroParsec (main) where
+
+import Data.Char
+import Control.Monad
+import Control.Applicative
+
+newtype Parser a = Parser { parse :: String -> [(a, String)] }
+
+runParser :: Parser a -> String -> a
+runParser m s =
+  case parse m s of
+    [(res, [])] -> res
+    [(_, rs)]   -> error "Parser did not consume entire stream."
+    _           -> error "Parser error."
+
+item :: Parser Char
+item = Parser $ \s ->
+  case s of
+    []       -> []
+    (c : cs) -> [(c, cs)]
+
+bind :: Parser a -> (a -> Parser b) -> Parser b
+bind p f = Parser $ \ s -> concatMap (\ (a, s') -> parse (f a) s') $ parse p s
+
+unit :: a -> Parser a
+unit a = Parser (\ s -> [(a, s)])
+
+combine :: Parser a -> Parser a -> Parser a
+combine p q = Parser (\ s -> parse p s ++ parse q s)
+
+failure :: Parser a
+failure = Parser (const [])
+
+option :: Parser a -> Parser a -> Parser a
+option p q = Parser $ \ s ->
+  case parse p s of
+    []  -> parse q s
+    res -> res
+
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = item `bind` \ c ->
+  if p c
+  then unit c
+  else Parser (const [])
+
+instance Functor Parser where
+  fmap f (Parser cs) = Parser (\ s -> [(f a, b) | (a, b) <- cs s])
+
+instance Applicative Parser where
+  pure = return
+  (Parser cs1) <*> (Parser cs2) = Parser (\ s -> [(f a, s2) | (f, s1) <- cs1 s, (a, s2) <- cs2 s1])
+
+instance Monad Parser where
+  return = unit
+  (>>=)  = bind
+
+instance MonadPlus Parser where
+  mzero = failure
+  mplus = combine
+
+instance Alternative Parser where
+  empty = mzero
+  (<|>) = option
+
+
+-- Combinators
+
+oneOf :: String -> Parser Char
+oneOf s = satisfy (`elem` s)
+
+chainl :: Parser a -> Parser (a -> a -> a) -> a -> Parser a
+chainl p op a = (p `chainl1` op) <|> return a
+
+chainl1 :: Parser a -> Parser (a -> a -> a) -> Parser a
+p `chainl1` op = p >>= rest
+  where rest a = (do f <- op
+                     b <- p
+                     rest (f a b))
+                 <|> return a
+
+char :: Char -> Parser Char
+char c = satisfy (c ==)
+
+natural :: Parser Integer
+natural = read <$> some (satisfy isDigit)
+
+string :: String -> Parser String
+string []     = return []
+string (c:cs) = char c >> string cs >> return (c:cs)
+
+spaces :: Parser String
+spaces = many $ oneOf " \n\r"
+
+token :: Parser a -> Parser a
+token p = do
+  a <- p
+  spaces
+  return a
+
+reserved :: String -> Parser String
+reserved s = token (string s)
+
+digit :: Parser Char
+digit = satisfy isDigit
+
+number :: Parser Int
+number = do
+  s  <- string "-" <|> return []
+  cs <- some digit
+  return $ read (s ++ cs)
+
+parens :: Parser a -> Parser a
+parens m = do
+  reserved "("
+  n <- m
+  reserved ")"
+  return n
+
+
+-- Calculator
+
+data Expr
+  = Add Expr Expr
+  | Mul Expr Expr
+  | Sub Expr Expr
+  | List Int
+  deriving Show
+
+eval :: Expr -> Int
+eval ex = case ex of
+  Add a b -> eval a + eval b
+  Mul a b -> eval a * eval b
+  Sub a b -> eval a - eval b
+  List n  -> n
+
+int :: Parser Expr
+int = do
+  n <- number
+  return (List n)
+
+expr :: Parser Expr
+expr = term `chainl1` addOp
+
+term :: Parser Expr
+term = factor `chainl1` mulOp
+
+factor :: Parser Expr
+factor = int <|> parens expr
+
+infixOp :: String -> (a -> a -> a) -> Parser (a -> a -> a)
+infixOp x f = reserved x >> return f
+
+addOp :: Parser (Expr -> Expr -> Expr)
+addOp = infixOp "+" Add <|> infixOp "-" Sub
+
+mulOp :: Parser (Expr -> Expr -> Expr)
+mulOp = infixOp "*" Mul
+
+run :: String -> Expr
+run = runParser expr
+
+main :: IO ()
+main = forever $ do
+  putStr "> "
+  a <- getLine
+  print $ eval $ run a
