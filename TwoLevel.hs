39014ba9fffe7c5551ce39c52c21624f4d34823c
Henry Till <henrytill@gmail.com>
2017-11-26 02:44:48 -0500

    update TwoLevel.hs

--- TwoLevel.hs
+++ TwoLevel.hs
@@ -1,4 +1,41 @@
+{-# LANGUAGE DeriveFunctor        #-}
+{-# LANGUAGE StandaloneDeriving   #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module TwoLevel where
+
+newtype Fix f = In { out :: f (Fix f) }
+
+deriving instance   Eq (f (Fix f)) =>   Eq (Fix f)
+deriving instance  Ord (f (Fix f)) =>  Ord (Fix f)
+deriving instance Show (f (Fix f)) => Show (Fix f)
+
+type Algebra f a = f a -> a
+
+cata :: Functor f => Algebra f a -> Fix f -> a
+cata alg = alg . fmap (cata alg) . out
+
+-- * A tree
+
+data T x
+  = Tip
+  | Leaf Int
+  | Fork x x
+  deriving (Eq, Show, Functor)
+
+type Tree = Fix T
+
+tip :: Tree
+tip = In Tip
+
+leaf :: Int -> Tree
+leaf x = In (Leaf x)
+
+fork :: Tree -> Tree -> Tree
+fork x y = In (Fork x y)
+
+example :: Tree
+example = fork (fork (leaf 1) (leaf 2)) (fork (leaf 3) tip)
 
 data RTree
   = RTip
@@ -6,34 +43,10 @@ data RTree
   | RFork RTree RTree
   deriving Show
 
-data T x
-  = Tip
-  | Leaf Int
-  | Fork x x
-  deriving Show
-
-data Tree = Wrap (T Tree) deriving Show
-
-unwrap :: Tree -> T Tree
-unwrap (Wrap x) = x
-
-tip :: Tree
-tip = Wrap Tip
-
-leaf :: Int -> Tree
-leaf x = Wrap (Leaf x)
-
-fork :: Tree -> Tree -> Tree
-fork x y = Wrap (Fork x y)
-
-convert :: Tree -> RTree
-convert t = case unwrap t of
-  Tip      -> RTip
-  Leaf x   -> RLeaf x
-  Fork l r -> RFork (convert l) (convert r)
-
-example :: Tree
-example = fork (fork (leaf 1) (leaf 2)) (fork (leaf 3) tip)
+convertAlg :: Algebra T RTree
+convertAlg Tip        = RTip
+convertAlg (Leaf x)   = RLeaf x
+convertAlg (Fork l r) = RFork l r
 
 rExample :: RTree
-rExample = convert example
+rExample = cata convertAlg example
