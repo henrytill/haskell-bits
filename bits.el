a7571ffeb03ac1ede7b97bf2fd95ed3d9bacefa6
Henry Till <henrytill@gmail.com>
2017-12-05 17:19:46 -0500

    update bits.el

--- bits.el
+++ bits.el
@@ -18,12 +18,19 @@
 (defun ht/run-hoogle ()
   (interactive)
   (ht/async-shell-command-in-nix-shell "hoogle server -p 8080")
+  (setq haskell-hoogle-url "http://localhost:8080/?hoogle=%s")
   (browse-url "http://localhost:8080"))
 
 (bind-map ht/playground-leader-map
   :keys ("M-m c")
   :evil-keys ("SPC c")
   :major-modes (haskell-mode))
+
+(ht/comment
+  (setq haskell-process-wrapper-function
+        (lambda (argv) (append (list "nix-shell" "--command" )
+                               (list (mapconcat 'identity argv " ")))))
+  nil)
 
 (bind-map-set-keys ht/playground-leader-map
   "d" 'ht/doctest
